2ef169269b8d1fdba0acc723b9f9ce939f4a4ccf
ckswnskfk <ckswnskfk@hanmail.net>
2023-06-03 21:59:49 +0900

    cloneDeep 함수 switch 문에서 if 문으로 변경, structuredClone 검사 추가

--- cloneDeep.js
+++ cloneDeep.js
@@ -5,36 +5,34 @@ const cloneDeep = (target) => {
     result = target;
   }
 
-  switch (target.constructor) {
-    case Array:
-      result = [];
-      for (let i = 0; i < target.length; i++) {
-        result[i] = cloneDeep(target[i]);
-      }
-      break;
-    case Map:
-      result = new Map();
-      for (let [key, value] of target) {
-        result.set(cloneDeep(key), cloneDeep(value));
-      }
-      break;
-    case Set:
-      result = new Set();
-      for (let value of target) {
-        result.add(cloneDeep(value));
-      }
-      break;
-    case Object:
-      result = {};
-      for (let prop in target) {
-        if (target.hasOwnProperty(prop)) {
-          result[prop] = cloneDeep(target[prop]);
-        }
-      }
-      break;
+  if (target.constructor === Array) {
+    result = [];
+    for (let i = 0; i < target.length; i++) {
+      result[i] = cloneDeep(target[i]);
+    }
+  }
 
-    default:
-      break;
+  if (target.constructor === Map) {
+    result = new Map();
+    for (let [key, value] of target) {
+      result.set(cloneDeep(key), cloneDeep(value));
+    }
+  }
+
+  if (target.constructor === Set) {
+    result = new Set();
+    for (let value of target) {
+      result.add(cloneDeep(value));
+    }
+  }
+
+  if (target.constructor === Object) {
+    result = {};
+    for (let prop in target) {
+      if (target.hasOwnProperty(prop)) {
+        result[prop] = cloneDeep(target[prop]);
+      }
+    }
   }
 
   return result;
--- cloneDeep.test.js
+++ cloneDeep.test.js
@@ -1,6 +1,6 @@
 import cloneDeep from "./cloneDeep";
 
-test("originObj랑 clonedObj랑 같니?", () => {
+describe("내가 만든 cloneDeep과 structuredClone()의 비교", () => {
   const originObj = {
     a: "abc",
     inner: {
@@ -11,16 +11,35 @@ test("originObj랑 clonedObj랑 같니?", () => {
   };
 
   const clonedObj = cloneDeep(originObj);
+  const clonedByStructuredClone = structuredClone(originObj);
 
-  /**
-   * 참조 타입은 같은 주소값을 가리키면 원본 데이터의 변조 위험이 있다.
-   * 같지 않은 경우 잘 복사가 된 것이므로 통과로 한다.
-   */
-  expect(clonedObj).toEqual(originObj);
-  expect(clonedObj).not.toBe(originObj);
-  expect(clonedObj.a).toBe(originObj.a);
-  expect(clonedObj.inner).not.toBe(originObj.inner);
-  expect(clonedObj.inner.arr).not.toBe(originObj.inner.arr);
-  expect(clonedObj.inner.map).not.toBe(originObj.inner.map);
-  expect(clonedObj.inner.set).not.toBe(originObj.inner.set);
+  test("originObj와 clonedObj의 속성이 같은 값을 가지는 지?", () => {
+    expect(clonedObj).toStrictEqual(originObj);
+  });
+
+  test("originObj와 clonedObj의 속성중 참조 타입인 값들이 같은 주소값을 가리키지는 않는지?", () => {
+    expect(clonedObj).not.toBe(originObj);
+    expect(clonedObj.inner).not.toBe(originObj.inner);
+    expect(clonedObj.inner.arr).not.toBe(originObj.inner.arr);
+    expect(clonedObj.inner.map).not.toBe(originObj.inner.map);
+    expect(clonedObj.inner.set).not.toBe(originObj.inner.set);
+  });
+
+  test("originObj와 clonedByStructuredClone의 속성이 같은 값을 가지는 지?", () => {
+    /**
+     * structuredClone은 toStrictEqual 로 검사했을 때 항상 fail 한다.
+     * 한 편, 일반 객체나 배열만 복사해 toEqual로 검사하면 pass 한다.
+     * 하지만 Map 이나 Set 타입을 복사해 toEqual로 검사하면 fail 한다.
+     * 이유는 잘 모르겠다..
+     */
+    expect(clonedByStructuredClone).toStrictEqual(originObj);
+  });
+
+  test("originObj와 clonedByStructuredClone의 속성중 참조 타입인 값들이 같은 주소값을 가리키지는 않는지?", () => {
+    expect(clonedByStructuredClone).not.toBe(originObj);
+    expect(clonedByStructuredClone.inner).not.toBe(originObj.inner);
+    expect(clonedByStructuredClone.inner.arr).not.toBe(originObj.inner.arr);
+    expect(clonedByStructuredClone.inner.map).not.toBe(originObj.inner.map);
+    expect(clonedByStructuredClone.inner.set).not.toBe(originObj.inner.set);
+  });
 });
